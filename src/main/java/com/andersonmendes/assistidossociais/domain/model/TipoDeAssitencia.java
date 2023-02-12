bcded17d2d454bdba36e9c67b73aac1a192e8b4a
Anderson Mendes <mendes.ams@gmail.com>
2023-02-12 02:37:55 -0300

    Correção no objeto TipoDeAssistencia, adicionando a anotação @Enumerated.

--- src/main/java/com/andersonmendes/assistidossociais/domain/model/TipoDeAssitencia.java
+++ src/main/java/com/andersonmendes/assistidossociais/domain/model/TipoDeAssitencia.java
@@ -5,6 +5,8 @@ import java.time.LocalDate;
 import org.springframework.format.annotation.DateTimeFormat;
 
 import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
@@ -20,24 +22,48 @@ public class TipoDeAssitencia {
 	@EqualsAndHashCode.Include
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
+	
+	@Enumerated(EnumType.STRING)
 	private FornecerAlimentos fornecerAlimentos;
+	
+	@Enumerated(EnumType.STRING)
 	private NecessitaConsultaMedica necessitaConsultaMedica;
+	
+	@Enumerated(EnumType.STRING)
 	private AviamentoReceitasMedicamentos aviamentoReceitasMedicamentos;
+	
+	@Enumerated(EnumType.STRING)
 	private VerificarEmprego verificarEmprego;
+	
+	@Enumerated(EnumType.STRING)
 	private NecessitaRoupas necessitaRoupas;
+	
+	@Enumerated(EnumType.STRING)
 	private NecessitaAssistenciaJuridica necessitaAssistenciaJuridica;
+	
+	@Enumerated(EnumType.STRING)
 	private EncaminhamentoAposentadoria encaminhamentoAposentadoria;
+	
+	@Enumerated(EnumType.STRING)
 	private EncaminhamentoSebem encaminhamentoSebem;
+	
+	@Enumerated(EnumType.STRING)
 	private Outros outros;
+	
 	private String quais;
+	
 	@DateTimeFormat(pattern = "yyyy-MM-dd")
 	private LocalDate sindicancia;
+	
 	@DateTimeFormat(pattern = "yyyy-MM-dd")
 	private LocalDate admissao;
+	
 	@DateTimeFormat(pattern = "yyyy-MM-dd")
 	private LocalDate promocao;
+	
 	@DateTimeFormat(pattern = "yyyy-MM-dd")
 	private LocalDate recusa;
+	
 	private String sindicanciaRealizadaPor; 
 	
 	
