0379ed8eae14f145e9ee4d494fbfe0914eef8ddd
Anderson Mendes <mendes.ams@gmail.com>
2023-06-18 22:03:42 -0300

    Criando exception NegocioException.

--- src/main/java/com/andersonmendes/assistidossociais/domain/model/Parecer.java
+++ src/main/java/com/andersonmendes/assistidossociais/domain/model/Parecer.java
@@ -5,14 +5,6 @@ import java.time.LocalDate;
 import org.springframework.format.annotation.DateTimeFormat;
 
 import jakarta.persistence.Embeddable;
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
-import jakarta.persistence.JoinColumn;
-import jakarta.persistence.ManyToOne;
-import lombok.Data;
-import lombok.EqualsAndHashCode;
 import lombok.Getter;
 import lombok.Setter;
 
