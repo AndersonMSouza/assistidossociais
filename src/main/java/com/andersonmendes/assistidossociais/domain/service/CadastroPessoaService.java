559ee2b58ce73ebc640ff9f677935f47a6a46ccd
Anderson Mendes <mendes.ams@gmail.com>
2023-06-20 23:40:25 -0300

    Criando método BuscarOuFalhar, criando exception PessoaNaoEncontradaException e refatorando método buscar.

--- src/main/java/com/andersonmendes/assistidossociais/domain/service/CadastroPessoaService.java
+++ src/main/java/com/andersonmendes/assistidossociais/domain/service/CadastroPessoaService.java
@@ -4,15 +4,19 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.PathVariable;
 
 import com.andersonmendes.assistidossociais.domain.exceptions.EntidadeEmUsoException;
-import com.andersonmendes.assistidossociais.domain.exceptions.EntidadeNaoEncontradaException;
+import com.andersonmendes.assistidossociais.domain.exceptions.PessoaNaoEncontradaException;
 import com.andersonmendes.assistidossociais.domain.model.Pessoa;
 import com.andersonmendes.assistidossociais.domain.repository.PessoaRepository;
 
 @Service
 public class CadastroPessoaService {
 
+	private static final String MSG_PESSOA_EM_USO 
+		= "Pessoa de código %d não pode ser removida pois está em uso!";
+	
 	@Autowired
 	private PessoaRepository pessoaRepository;
 	
@@ -24,13 +28,17 @@ public class CadastroPessoaService {
 		try {
 			pessoaRepository.deleteById(pessoaId);
 		} catch (EmptyResultDataAccessException e) {
-			throw new EntidadeNaoEncontradaException(
-					String.format("Não existe pessoa cadastrada com o codigo %d.", pessoaId));
+			throw new PessoaNaoEncontradaException(pessoaId);
 		
 		} catch (DataIntegrityViolationException e) {
 			throw new EntidadeEmUsoException(
-				String.format("Pessoa de código %d não pode ser removida pois está em uso!", pessoaId));
+				String.format(MSG_PESSOA_EM_USO, pessoaId));
 		}
+	}
+	
+	public Pessoa buscarOuFalhar(@PathVariable Long pessoaId) {
+		return pessoaRepository.findById(pessoaId)
+			.orElseThrow(() -> new PessoaNaoEncontradaException(pessoaId));
 	}
 	
 }
