273159644a516c0a41048dca7a492a64794b680a
Anderson Mendes <mendes.ams@gmail.com>
2023-06-26 18:30:13 -0300

    Refatorando método adicionar.

--- src/main/java/com/andersonmendes/assistidossociais/api/controller/DependentesController.java
+++ src/main/java/com/andersonmendes/assistidossociais/api/controller/DependentesController.java
@@ -14,6 +14,7 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.andersonmendes.assistidossociais.domain.exceptions.EntidadeEmUsoException;
@@ -43,14 +44,9 @@ public class DependentesController {
 	}
 	
 	@PostMapping
-	public ResponseEntity<?> adicionar(@RequestBody Dependente dependente) {
-		try {
-			dependente = cadastroDependenteService.salvar(dependente);
-			return ResponseEntity.status(HttpStatus.CREATED).body(dependente);
-			
-		} catch (EntidadeNaoEncontradaException e) {
-			return ResponseEntity.badRequest().body(e.getMessage());
-		}
+	@ResponseStatus(HttpStatus.CREATED)
+	public Dependente adicionar(@RequestBody Dependente dependente) {
+		return cadastroDependenteService.salvar(dependente);
 	}
 	
 	@PutMapping("{dependenteId}")
